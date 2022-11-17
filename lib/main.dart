402c3a6e7692e334b9d3a2fbb4c4aef0a813bdbc
cwh211 <118464746+cwh211@users.noreply.github.com>
2022-11-18 00:13:37 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,203 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+final player=AudioPlayer();
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  final tabs=[
+    Center(child: screen1()),
+    Center(child: screen2()),
+    Center(child: screen3()),
+    Center(child: screen4()),
+  ];
+
+  int _currentindex=0;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Midterm'),),
+        body: tabs[_currentindex],
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Colors.blue,
+          selectedItemColor: Colors.white,
+          selectedFontSize: 18.0,
+          unselectedFontSize: 14.0,
+          iconSize: 30.0,
+          currentIndex: _currentindex,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home),
+                                    label: 'Home',
+                                    ),
+            BottomNavigationBarItem(icon: Icon(Icons.access_alarm),
+                                    label: 'Alarm',
+                                    ),
+            BottomNavigationBarItem(icon: Icon(Icons.business),
+                                    label: 'Business',
+                                    ),
+            BottomNavigationBarItem(icon: Icon(Icons.school),
+                                    label: 'School',
+                                    ),
+          ],
+          onTap: (index) { setState(() {
+                                       _currentindex=index;
+                                       if (index!=0) {
+                                         player.stop();
+                                       }
+                         });
+        },
+      ),
+      ),
+    );
+  }
+}
+
+class screen1 extends StatelessWidget {
+
+  final String s1='我叫惟翔，出生在一般的家庭，我哥哥目前剛上研究所，我立志當social King';
+
+  @override
+  Widget build(BuildContext context) {
+
+    player.play(AssetSource("Free_Test_Data_1MB_MP3.mp3"));
+
+    return SingleChildScrollView(
+      child: Column(
+        children:<Widget>[
+          //先放個標題
+          Padding(padding: EdgeInsets.fromLTRB(20, 30, 20, 20),
+            child: Text("Who am I",
+                style: TextStyle(fontSize:24,
+                  fontWeight:FontWeight.bold,)),
+          ),
+          //文字自傳部份
+          Container(
+            padding: EdgeInsets.all(20),
+            margin: EdgeInsets.fromLTRB(30, 0, 30, 50),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black, width: 3),
+              borderRadius: BorderRadius.circular(8),
+              boxShadow: [ BoxShadow(color: Colors.amberAccent,
+                  offset: Offset(6, 6)),
+              ],),
+            child:Text(s1,
+              style: TextStyle(fontSize: 20),),
+          ),
+
+          //放一張照片
+          Container(
+            color: Colors.redAccent,
+            child: Image.asset('images/f1.jpg'),
+            height: 200,
+            width: 200,
+          ),
+          SizedBox(height: 30,),
+
+          //一列放兩個圖
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                width: 150,
+                height: 150,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 2,
+                    color: Colors.purple,
+                    style: BorderStyle.solid,
+                  ),
+                  borderRadius: BorderRadius.circular(30),
+                  image: DecorationImage(
+                    image: AssetImage('images/f1.jpg'),
+                    fit: BoxFit.cover ,
+                  ),
+                  color: Colors.white,
+                ),
+              ),
+              Container(
+                width: 150,
+                height: 150,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 2,
+                    color: Colors.purple,
+                    style: BorderStyle.solid,
+                  ),
+                  borderRadius: BorderRadius.circular(30),
+                  image: DecorationImage(
+                    image: AssetImage('images/f1.jpg'),
+                    fit: BoxFit.cover ,
+                  ),
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class screen2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(child:Text('學習歷程'),);
+  }
+}
+
+class screen3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child:Column(
+        children: [
+          //有多種排版方式, 此處以最直覺的方式將每一列放文字內容
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [Text("大一時期"),],
+          ),
+          SizedBox(height: 10,),
+          Row(mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                    height: 200,
+                    width: 200,
+                  child: ListView(
+                    children: [
+                      //條列式參考
+                      Text('1. 學好英文'),
+                      Text('2. 組合語言'),
+                      Text('3. 考取證照'),
+                      Text('4. 人際關係'),
+                    ],
+                  ),
+                ),
+              ],),
+          Row(),
+          Row(),
+          Row(),
+          Row(),
+          Row(),
+          Row(),
+        ],
+      ),);
+  }
+}
+
+class screen4 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(child:Text('專案方向'),);
+  }
+}
